d870042b3ec40a3bb06595b20487146ea0aa1f40
hjoy13 <hjoy0341@gmail.com>
2026-08-22 02:44:19 +0600

    feat: register custom user in admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("SupportDesk", {"fields": ("role",)}), #tupple
+    )
+
+    # here (SupportDesk, something) -> tuple
+    # SupportDesk -> string 
+    # {"fields":("role",)} ->dictionary
+    # inside dictionary fields -> key, role->value
+    #("role",) also a tuple, comma matters in tuple, 
+    #without comma ("role") will be treated as string 
+
+    #fieldsets -> editing an existing user
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("SupportDesk", {"fields": ("role",)}),
+    )
+
+    #add_fieldsets -> creating a new user
